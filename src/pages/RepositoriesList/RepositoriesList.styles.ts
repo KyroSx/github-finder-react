0e6e31fd8427aa74d93b304062de1ac4869759a1
kyroSx <leolub150@gmail.com>
2023-06-27 14:50:14 -0300

    feat: improve styles for search bar

--- src/pages/RepositoriesList/RepositoriesList.styles.ts
+++ src/pages/RepositoriesList/RepositoriesList.styles.ts
@@ -12,7 +12,7 @@ export const Header = styled.header`
 `;
 
 export const Content = styled.div`
-  max-width: 600px;
+  max-width: 900px;
   margin: 0 auto;
   padding: 16px;
 `;
@@ -21,17 +21,47 @@ export const SearchBar = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 16px;
+
+  gap: 10px;
 `;
 
 export const Input = styled.input`
   flex: 1;
-  padding: 8px;
+  padding: 16px;
+
+  font-size: 22px;
+  border: 1px solid ${({ theme }) => theme.colors.background2};
+  border-radius: 6px;
+  outline: none;
+  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary};
+    box-shadow: 0 0 0 3px rgba(3, 102, 214, 0.3);
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.background};
+    cursor: not-allowed;
+  }
 `;
 
 export const Button = styled.button`
-  background-color: ${({ theme }) => theme.colors.green};
+  font-size: 22px;
+  padding: 14px 20px;
+  font-weight: bold;
   color: ${({ theme }) => theme.colors.background_light};
-  padding: 8px 16px;
+  background-color: ${({ theme }) => theme.colors.green};
   border: none;
+  border-radius: 6px;
   cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.green_bold};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    cursor: not-allowed;
+  }
 `;
--- src/ui/ThemeProvider/theme.ts
+++ src/ui/ThemeProvider/theme.ts
@@ -1,12 +1,17 @@
 export const theme = {
   colors: {
     background: '#f6f8fa',
+    background2: '#e1e4e8',
     background_light: '#fff',
+
     text: '#24292e',
     primary: '#0366d6',
     secondary: '#6a737d',
 
     green: '#28a745',
-    red: 'red',
+    green_bold: '#218838',
+  },
+  fonts: {
+    primary: '',
   },
 };
