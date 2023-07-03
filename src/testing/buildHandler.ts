45bf0c00ff08acfd4b8f97ee450a1737966def3a
kyroSx <leolub150@gmail.com>
2023-07-03 18:17:19 -0300

    refactor: split code into functions

--- src/testing/buildHandler.ts
+++ src/testing/buildHandler.ts
@@ -1,38 +1,56 @@
 import { DefaultBodyType, rest } from 'msw';
 
-type Handler<T> =
-  | {
-      url: string;
-      method: keyof typeof rest;
-      paginated?: false;
-      response: T;
-    }
-  | {
-      url: string;
-      method: keyof typeof rest;
-      paginated: true;
-      paginatedResponse: Array<{ page: number; response: T }>;
-    };
+type NonePaginatedHandler<T> = {
+  url: string;
+  method: keyof typeof rest;
+  paginated?: false;
+  response: T;
+};
+
+type PaginatedHandler<T> = {
+  url: string;
+  method: keyof typeof rest;
+  paginated: true;
+  paginatedResponse: Array<{ page: number; response: T }>;
+};
+
+type Handler<T> = NonePaginatedHandler<T> | PaginatedHandler<T>;
 
 export function buildHandler<T extends DefaultBodyType>(props: Handler<T>) {
-  if (props.paginated) {
-    const responses = props.paginatedResponse.map((item) => ({
-      ...item,
-      url: props.url.replace(':page', item.page.toString()),
-    }));
+  return props.paginated
+    ? buildPaginatedHandler(props)
+    : buildNonPaginatedHandler(props);
+}
 
-    return rest[props.method](props.url, (req, res, ctx) => {
-      const pageParam = req.url.searchParams.get('page');
-      const page = pageParam ? parseInt(pageParam, 10) : 1;
+function buildPaginatedHandler<T extends DefaultBodyType>(
+  props: PaginatedHandler<T>
+) {
+  const responses = props.paginatedResponse.map((item) => ({
+    ...item,
+    url: props.url.replace(':page', item.page.toString()),
+  }));
 
-      const matchedResponse = responses.find((item) => item.page === page);
+  return rest[props.method](props.url, (req, res, ctx) => {
+    const pageParam = req.url.searchParams.get('page');
+    const page = pageParam ? parseInt(pageParam, 10) : 1;
 
-      if (matchedResponse) {
-        return res(ctx.delay(), ctx.json(matchedResponse.response));
-      }
+    const matchedResponse = responses.find((item) => item.page === page);
 
-      return res(ctx.status(404));
-    });
+    if (matchedResponse) {
+      return res(ctx.delay(), ctx.json(matchedResponse.response));
+    }
+
+    return res(ctx.status(404));
+  });
+}
+
+function buildNonPaginatedHandler<T extends DefaultBodyType>(
+  props: NonePaginatedHandler<T>
+) {
+  if (!props.response) {
+    throw new Error(
+      'A propriedade "response" é obrigatória quando "paginated" é false.'
+    );
   }
 
   return rest[props.method](props.url, (req, res, ctx) => {
